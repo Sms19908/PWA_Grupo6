ee2f7fb93419a2c023c9b3e553fce23118bb6a0b
SaulMerloS <smerlo2014@gmail.com>
2025-04-07 08:19:34 -0600

    Modelos de la BD

---
+++ SC-701-PAW-Lunes/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SC_701_PAW_Lunes.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id_Cart { get; set; }
+        public int Id_User { get; set; }
+        public int Id_Inv { get; set; }
+        public String NombreProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Monto { get; set; }
+
+    }
+}
---
+++ SC-701-PAW-Lunes/Models/Favorites.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SC_701_PAW_Lunes.Models
+{
+    public class Favorites
+    {
+        [Key]
+        public int Id_Favorites { get; set; }
+        public int Id_User { get; set; }
+        public int Id_Inv {  get; set; }
+        public string NombreProducto { get; set; }
+        public decimal PrecioProducto { get; set; }
+    }
+}
