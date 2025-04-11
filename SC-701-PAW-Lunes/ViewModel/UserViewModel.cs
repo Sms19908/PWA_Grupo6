76335efd1b4bf56dbab262ef77e6f33f60f7e0c3
MelanyAC <106123847+MelanyAC@users.noreply.github.com>
2025-04-11 04:54:57 +0000

    Agregando modelo para usuario en tabla de administracion

---
+++ SC-701-PAW-Lunes/ViewModel/UserViewModel.cs
@@ -0,0 +1,11 @@
+namespace SC_701_PAW_Lunes.ViewModel
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Roles { get; set; }
+        public bool IsCurrentUser { get; set; }
+    }
+}
